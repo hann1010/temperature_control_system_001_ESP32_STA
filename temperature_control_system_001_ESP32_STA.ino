b4fc5f1ba2eac8991e793aa701b00e940ac861bc
Hanna <hanna.paivasto@gmail.com>
2026-06-10 20:25:15 +0300

    pre code

---
+++ temperature_control_system_001_ESP32_STA.ino
@@ -0,0 +1,14 @@
+// Station WiFi Connection for temperature control 001
+// use with temperature_control_system_2-001_Mega-2560
+// en Hanna P 2026
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
