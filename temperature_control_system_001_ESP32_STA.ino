01576599496bddf9c45174be55c282e756869d0f
Hanna <hanna.paivasto@gmail.com>
2026-07-29 22:43:39 +0300

    pre code

--- temperature_control_system_001_ESP32_STA.ino
+++ temperature_control_system_001_ESP32_STA.ino
@@ -91,6 +91,8 @@ void loop() {
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
           }
         }
       }
