834a1ed03ea2eb2cf5c1f6a9170c6e1c974b2ee4
Hanna <hanna.paivasto@gmail.com>
2026-07-31 23:17:33 +0300

    pre code

--- temperature_control_system_001_ESP32_STA.ino
+++ temperature_control_system_001_ESP32_STA.ino
@@ -101,6 +101,9 @@ void loop() {
             // palce for functional code
 
             // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            
           }
         }
       }
