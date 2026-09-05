b0ad4ebe8aad5ad776e45cd76ad7d8a6d5cd7025
Hanna <hanna.paivasto@gmail.com>
2026-09-05 22:37:22 +0300

    pre code

--- temperature_control_system_001_ESP32_STA.ino
+++ temperature_control_system_001_ESP32_STA.ino
@@ -148,6 +148,7 @@ void loop() {
             
             // Break out of the while loop
             break;
+            
           } else { // if you got a newline, then clear currentLine
         }
       }
